ed71dc642138480140957437496639e30d770543
cristiangutierrz <57638809+cristiangutierrz@users.noreply.github.com>
2022-05-26 13:32:57 +0200

    Codi comentat

--- static/script.js
+++ static/script.js
@@ -1,35 +1,18 @@
 'use strict';
 
-window.addEventListener('load', function () {
-  // Fer algo quan la pagina carregui
-});
 
-if ( window.history.replaceState ) {
-  // Evita prompt del navegador quan es fa refresc amb POST actiu
-  window.history.replaceState( null, null, window.location.href );
-}
-
-let out = document.getElementsByClassName("editable")[0]
-console.log(out.innerHTML)
-
+// Quan fem click sobre una paraula vermella mostrem una finestra (element HTML dialog)
 function showwords(params, ctx, idx) {
-  console.log(params) 
-  console.log(ctx.style )
-  console.log(idx)
-
-  // ctx.innerHTML = params[0]
-  // ctx['style']="text-decoration: none; color:rgb(17, 127, 0); font-weight: 600"  ;
-
   document.getElementsByClassName(idx)[1].showModal(); 
 }
 
+// Quan fem click sobre una paraula sugerida la corregim automaticament
 function correctword(ctx, idx, parent) {
-
-  // document.getElementsByClassName(idx)[1].remove();
   ctx.parentNode.parentNode.parentNode.style="text-decoration: none; color:rgb(17, 127, 0); font-weight: 600"  ; 
   ctx.parentNode.parentNode.parentNode. innerHTML = ctx.innerHTML + " "
 }
 
+// Tanquem la finestra de sugerencies, aixo es farà quan donem click a `Close`
 function closeModal(ctx) {
   console.log(ctx.parentNode)
   ctx.parentNode.close()
